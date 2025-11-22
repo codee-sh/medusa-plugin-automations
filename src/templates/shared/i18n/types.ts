7066ea7ffeb4b545fd519b7f87f61dfef7e25bcb
Krzysztof Polak <chris@iamcodee.co>
2025-11-22 14:00:11 +0100

    - add mockups for emails preview

--- src/templates/shared/i18n/types.ts
+++ src/templates/shared/i18n/types.ts
@@ -1,11 +1,4 @@
 import type { Locale } from "date-fns"
-// import enUS from "./translations/en.json"
-
-// const resources = {
-//   translation: enUS,
-// } as const
-
-// export type Resources = typeof resources
 
 export type Language = {
   code: string
