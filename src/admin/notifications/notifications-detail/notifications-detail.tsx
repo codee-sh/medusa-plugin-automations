ea32f9c6d5975e835bd4c79b5a4fcf8ee8207981
Krzysztof Polak <chris@iamcodee.co>
2025-11-24 16:40:30 +0100

    Refactor NotificationsDetail component to replace OrderActions and OrderPaymentActions with NotificationOrder and NotificationOrderPayment components, streamlining notification handling for orders and payments.

--- src/admin/notifications/notifications-detail/notifications-detail.tsx
+++ src/admin/notifications/notifications-detail/notifications-detail.tsx
@@ -1,39 +1,17 @@
 import { Container, Heading, Text } from "@medusajs/ui"
 import { useTranslation } from "react-i18next"
 import { useEvents } from "../../../hooks/api/events"
-import { Actions as OrderActions } from "./components/order-actions"
-import { Actions as OrderPaymentActions } from "./components/order-payment-actions"
+import { NotificationOrder } from "./notification-order"
+import { NotificationOrderPayment } from "./notification-order-payment"
 
 export const NotificationsDetail = ({ type, data }: { type: string, data: any }) => {
-  const { mutate: sendEvent, isPending, isError, data: eventsData } = useEvents()
-
-  const handleSendOrderNotification = ({ name, trigger_type }: { name: string, trigger_type: string }) => {
-    sendEvent({
-      name: name,
-      data: {
-        id: data.id,
-        trigger_type: trigger_type,
-      },
-    })
-  }
-
-  const handleSendPaymentNotification = ({ name, trigger_type }: { name: string, trigger_type: string }) => {
-    sendEvent({
-      name: name,
-      data: {
-        id: data?.payment_collections[0]?.payments[0]?.id,
-        trigger_type: trigger_type,
-      },
-    })
-  }
-
   return (
     <Container className="p-0">
       <Header />
       {type === "order" && (
         <>
-          <OrderActions onSend={handleSendOrderNotification} isPending={isPending} isError={isError} eventsData={eventsData} />
-          <OrderPaymentActions onSend={handleSendPaymentNotification} isPending={isPending} isError={isError} eventsData={eventsData} />
+          <NotificationOrder data={data} />
+          <NotificationOrderPayment data={data} />
         </>
       )}
     </Container>
