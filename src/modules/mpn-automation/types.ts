a88ea1a6dafca712a7c78e509dd67f3db0c7d31d
Krzysztof Polak <chris@iamcodee.co>
2025-12-01 09:31:14 +0100

    Add inventory level attributes and update event inventory types to use them

--- src/modules/mpn-automation/types.ts
+++ src/modules/mpn-automation/types.ts
@@ -149,21 +149,44 @@ export const INVENTORY_ITEM_ATTRIBUTES = [
   },
 ]
 
+export const INVENTORY_LEVEL_ATTRIBUTES = [
+  {
+    value: "inventory_level.stocked_quantity",
+    label: "Stocked Quantity"
+  },
+  {
+    value: "inventory_level.reserved_quantity",
+    label: "Reserved Quantity"
+  },
+  {
+    value: "inventory_level.available_quantity",
+    label: "Available Quantity"
+  },
+  {
+    value: "inventory_level.incoming_quantity",
+    label: "Incoming Quantity"
+  },
+  {
+    value: "inventory_level.location_id",
+    label: "Location ID"
+  },
+]
+
 export const EVENT_INVENTORY_TYPES = [
   {
     value: "inventory.inventory-level.created",
     label: "Inventory Level Created",
-    attributes: INVENTORY_ITEM_ATTRIBUTES
+    attributes: INVENTORY_LEVEL_ATTRIBUTES
   },
   {
     value: "inventory.inventory-level.updated",
     label: "Inventory Level Updated",
-    attributes: INVENTORY_ITEM_ATTRIBUTES
+    attributes: INVENTORY_LEVEL_ATTRIBUTES
   },
   {
     value: "inventory.inventory-level.deleted",
     label: "Inventory Level Deleted",
-    attributes: INVENTORY_ITEM_ATTRIBUTES
+    attributes: INVENTORY_LEVEL_ATTRIBUTES
   }
 ]
 
